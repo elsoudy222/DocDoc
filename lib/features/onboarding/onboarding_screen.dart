51dc7aaa920d173d99a8e345b5f5fa5632e9fe7f
elsoudy222 <elsoudy222@gmail.com>
2024-11-12 02:28:18 +0400

    - Made file for doc app client. - Added Navigation Extension. - Added routing folder that contain app router and routes files. - Added colors & text styles. -Added empty login screen. -Added onboarding screen

---
+++ lib/features/onboarding/onboarding_screen.dart
@@ -0,0 +1,45 @@
+import 'package:doc_doc/features/onboarding/widgets/doc_logo_and_name.dart';
+import 'package:doc_doc/features/onboarding/widgets/doctor_image_and_text.dart';
+import 'package:doc_doc/features/onboarding/widgets/get_started_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../core/theming/text_styles.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.0.h,bottom: 30.0.h),
+            child:  Column(
+              children: [
+                const DocLogoAndName(),
+                SizedBox(height: 20.h,),
+                const DoctorImageAndText(),
+                Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 30.0.w),
+                  child:  Column(
+                    children: [
+                      Text(
+                        "Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.",
+                        style: TextStyles.font13GrayRegular,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 30.h,),
+                      const GetStartedButton(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
